fdabab10f06b71a56112b16a46fe2001b8b79460
danadrianmirea <152083571+danadrianmirea@users.noreply.github.com>
2024-10-17 12:42:08 +0300

    Delete init.bat

--- init.bat
+++
@@ -1,7 +0,0 @@
-echo "# cherno_cpptut_follow_along" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin git@github.com:danadrianmirea/cherno_cpptut_follow_along.git
-git push -u origin main
