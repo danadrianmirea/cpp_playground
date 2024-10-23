e78e41f19842a8846f3c9b395039e7d08fc0f6f0
Dan Adrian Mirea <damirea84@gmail.com>
2024-10-23 16:37:25 +0300

    minor formatting

--- code/main.cpp
+++ code/main.cpp
@@ -1,21 +1,16 @@
 #include <cstdint>
 #include <iostream>
-
 #include "example.h"
 #include "datastructures.h"
 #include "testclass.h"
-
 #include "log.h"
 #include "tokenizer.h"
-
 #include "staticmember.h"
-
 #include "generictest.hpp"
 
 using namespace std;
 
 int32_t main()
 {
-  Log::LogTest();
   return 0;
 }
