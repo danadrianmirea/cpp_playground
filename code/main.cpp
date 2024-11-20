67f56a17e6ef47b48b1ce173482f6fd36dd3650d
Dan Adrian Mirea <damirea84@gmail.com>
2024-11-20 16:15:47 +0200

    modified:   code/main.cpp

--- code/main.cpp
+++ code/main.cpp
@@ -12,7 +12,32 @@
 
 using namespace std;
 
+void (*funcPtr)(int);
+
+void realFunction(int n)
+{
+  std::cout << "n is: \n" << n;
+}
+
 int32_t main()
 {
+  int num = 8;
+  int &ref = num;
+  int const &cRef = num;
+
+  funcPtr = realFunction;
+  
+  int *intPtr = new int;
+  int* const ptrToConstInt = new int;
+  
+  *intPtr = num;
+  *ptrToConstInt = num;
+  *ptrToConstInt = 13;
+  
+  //cout << "b is: " << *c;
+  delete 
+
+
   return 0;
 }
+
