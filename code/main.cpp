98453114e3d106a40e9e3f17c3841293f8820e8b
Dan Adrian Mirea <damirea84@gmail.com>
2024-11-20 17:57:25 +0200

    cleanup main

--- code/main.cpp
+++ code/main.cpp
@@ -1,13 +1,11 @@
 #include <cstdint>
 #include <iostream>
 #include "bitwise.h"
-#include "constAndPointers.h"
 
 using namespace std;
 
 int32_t main()
 {
-  ConstPointerTestFunction();
   return 0;
 }
 
