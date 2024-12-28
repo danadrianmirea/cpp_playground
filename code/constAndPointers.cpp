25808054775a7da39a70672cd42dfb4a3677178f
Dan Adrian Mirea <damirea84@gmail.com>
2024-12-28 12:33:15 +0200

    add test code

--- code/constAndPointers.cpp
+++ code/constAndPointers.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include "constAndPointers.h"
 
-
 void (*funcPtr)(int);
 
 void realFunction(int n)
@@ -19,21 +18,21 @@ void ConstPointerTestFunction()
   int num = 8;
   int num2 = 11;
   const int cNum = 13;
-  int &ref = num;
-  int const &cRef = num;
-  const int &cRef2 = num;
-  const int const &cRef3 = num;
-  const const const int gigaConst = 8; // valid but same as const int
+  int& ref = num;
+  int const& cRef = num;
+  const int& cRef2 = num;
+  const int const& cRef3 = num;
+  const const const int gigaConst = 8; // valid but same as const int (actually seems to depend on compiler and settings)
   const const const int const const superGigaConst = 111; // valid but same as const int 
 
-  int *intPtr = new int;
+  int* intPtr = new int;
   int* const constantPointer = &num;
   int const* pointerToConst = &num;
   int const* const constantPointerToConst = &num;
 
   funcPtr = realFunction;
   funcPtr(103);
-  
+
   *intPtr = num;
   *intPtr = 14;
   Print(num);
@@ -60,11 +59,9 @@ void ConstPointerTestFunction()
   pointerToConst = &num2;
   Print(*pointerToConst);
   //*pointerToConst = 7777; // invalid, this is a pointer to a constant int
-
   //constantPointerToConst = &num2; // invalid, cannot change a constant pointer value
   //*constantPointerToConst = 8888; // invalid, cannot change the value the pointer is pointing to because it's a pointer TO a const
   // essentially constantPointerToConst is just set once to some memory and can only read it's value
-
   //cRef = 88; // not valid, this reference is const
   //cRef2 = 99; // same as above, just defined differently: const int and int const are the same
 
--- code/main.cpp
+++ code/main.cpp
@@ -1,9 +1,12 @@
 #include <cstdint>
 #include <iostream>
 
+#include "epochConvert.h"
+
 using namespace std;
 
 int32_t main()
 {
+  TestEpochConvert();
   return 0;
 }
