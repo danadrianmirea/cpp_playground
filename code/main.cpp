0b6fc8f6958a9baad688d62c6e723541833f3915
Dan Adrian Mirea <damirea84@gmail.com>
2025-01-06 03:29:56 +0200

    smart pointer test

--- code/main.cpp
+++ code/main.cpp
@@ -3,7 +3,21 @@
 
 using namespace std;
 
+class ExampleClass
+{
+public:
+  ExampleClass() : val(0) { std::cout << "ExampleClass ct\n"; };
+  ~ExampleClass() { std::cout << "ExampleClass dt\n"; }
+  void SetVal(const int n) { val = n; }
+  int GetVal() { return val; }
+private:
+  int val;
+};
+
 int32_t main()
 {
+  std::unique_ptr<ExampleClass> g(new ExampleClass());
+  g->SetVal(7);
+  std::cout << g->GetVal()  << "\n";
   return 0;
-}
+} 
