bf6556ab6d80fbdc2adb1c2b3f59858dd39129c0
Dan Adrian Mirea <damirea84@gmail.com>
2025-02-20 19:53:48 +0200

    add sfinae using constexpr

---
+++ code/sfinae_using_constexpr.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <iostream>
+#include <type_traits>
+
+template <typename T>
+void print(T value) {
+    if constexpr (std::is_integral_v<T>) {
+        std::cout << "Integral: " << value << std::endl;
+    } else if constexpr (std::is_floating_point_v<T>) {
+        std::cout << "Floating point: " << value << std::endl;
+    } else {
+        std::cout << "Other type: " << value << std::endl;
+    }
+}
+
+int TestSfinaeCPP17() {
+    print(42);        // Calls integral branch
+    print(3.14);      // Calls floating-point branch
+    print("hello");   // Calls other type branch
+    return 0;
+}
