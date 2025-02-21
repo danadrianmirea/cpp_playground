c107db9aba26ddcb5ed53e574f1f31992383841a
Dan Adrian Mirea <damirea84@gmail.com>
2025-02-22 00:49:39 +0200

    update sfinae example

--- code/main.cpp
+++ code/main.cpp
@@ -3,7 +3,10 @@
 
 using namespace std;
 
+#include "sfinae_old.hpp"
+
 int32_t main()
 {
+  TestCPP11_Sfinae();
   return 0;
 }
--- code/sfinae_old.hpp
+++ code/sfinae_old.hpp
@@ -5,20 +5,19 @@
 
 // Enable this function only if T is an integral type
 template <typename T>
-typename std::enable_if<std::is_integral<T>::value, void>::type print(T value) 
+typename std::enable_if_t<std::is_integral_v<T>, void> print(T value)
 {
   std::cout << "Integral: " << value << std::endl;
 }
 
 // Enable this function only if T is a floating-point type
 template <typename T>
-typename std::enable_if<std::is_floating_point<T>::value, void>::type
-print(T value) 
+typename std::enable_if_t<std::is_floating_point_v<T>, void> print(T value)
 {
   std::cout << "Floating point: " << value << std::endl;
 }
 
-int TestCPP11_Sfinae() 
+int TestCPP11_Sfinae()
 {
   print(42);
   print(3.14);
