7c4c7f0bb92afe3d5acbeb06c075b92dd26065b4
Dan Adrian Mirea <damirea84@gmail.com>
2025-02-21 19:34:22 +0200

    cleanup main

--- code/main.cpp
+++ code/main.cpp
@@ -3,22 +3,8 @@
 
 using namespace std;
 
-template <typename T>
-typename std::enable_if<std::is_integral<T>::value, void>::type Print(T val)
-{
-  std::cout << "Integer value: " << val << "\n";
-}
-
-template <typename T>
-typename std::enable_if<std::is_floating_point<T>::value, void>::type Print(T val)
-{
-  std::cout << "Float value: " << val << "\n";
-}
-
 int32_t main()
 {
-  Print(3);
-  Print(5.3f);
   return 0;
 }
 
