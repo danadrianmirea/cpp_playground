d0f5552e0e75380e2ef65d5da9449f23db66d86a
Dan Adrian Mirea <damirea84@gmail.com>
2025-02-21 13:41:20 +0200

    add longestpalindrome

--- code/main.cpp
+++ code/main.cpp
@@ -3,7 +3,34 @@
 
 using namespace std;
 
+string longestPalindrome(string s)
+{
+  int n = s.size();
+  if (n <= 1) return s;
+
+  for (int size = n; size >= 1; --size)
+  {
+    for (int i = 0; i + size <= n; ++i)
+    {
+      string sub = s.substr(i, size);
+      string rev = sub;
+      reverse(rev.begin(), rev.end());
+
+      if (sub == rev)
+      {
+        return sub;
+      }
+    }
+  }
+  return {};
+}
+
+
 int32_t main()
 {
+  std::string str("cbbd");
+  std::cout << longestPalindrome(str);
   return 0;
-} 
+}
+
+
