acc2aa83ddf86c5fb416f3a28980854daf145b0f
Dan Adrian Mirea <damirea84@gmail.com>
2025-02-24 21:25:33 +0200

    update longest palindrome with more optimised solution

--- code/longestPalindromeSubstr.h
+++ code/longestPalindromeSubstr.h
@@ -1,28 +1,34 @@
 #pragma once
 
-#include <string>
 #include <algorithm>
+#include <string>
+
 
 using namespace std;
 
-string longestPalindrome(string s)
-{
-    int n = s.size();
-    if (n <= 1) return s;
+string longestPalindrome(string s) {
+  int n = s.size();
+  if (n <= 1)
+    return s;
 
-    for (int size = n; size >= 1; --size)
-    {
-        for (int i = 0; i + size <= n; ++i)
-        {
-            string sub = s.substr(i, size);
-            string rev = sub;
-            reverse(rev.begin(), rev.end());
+  for (int size = n; size >= 1; --size) {
+    for (int i = 0; i + size <= n; ++i) {
+      int left = i, right = i + size - 1;
+      bool isPalindrome = true;
 
-            if (sub == rev)
-            {
-                return sub;
-            }
+      while (left < right) {
+        if (s[left] != s[right]) {
+          isPalindrome = false;
+          break;
         }
+        ++left;
+        --right;
+      }
+
+      if (isPalindrome) {
+        return s.substr(i, size);
+      }
     }
-    return {};
+  }
+  return {};
 }
