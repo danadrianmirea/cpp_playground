a9116035a34d0762d44aac8609e0dfcbee8bab13
Dan Adrian Mirea <damirea84@gmail.com>
2025-06-04 20:51:55 +0300

    cleanup main

--- code/main.cpp
+++ code/main.cpp
@@ -3,29 +3,7 @@
 
 int main()
 {
-    HuffmanEncoding huffman;
-
-std::string text = 
-    "The quick brown fox jumps over the lazy dog. This sentence contains every letter in the English alphabet. "
-    "The quick brown fox jumps over the lazy dog. This sentence contains every letter in the English alphabet. "
-    "The quick brown fox jumps over the lazy dog. This sentence contains every letter in the English alphabet. "
-    "The quick brown fox jumps over the lazy dog. This sentence contains every letter in the English alphabet. "
-    "The quick brown fox jumps over the lazy dog. This sentence contains every letter in the English alphabet. "
-    "The quick brown fox jumps over the lazy dog. This sentence contains every letter in the English alphabet. "
-    "The quick brown fox jumps over the lazy dog. This sentence contains every letter in the English alphabet. "
-    "The quick brown fox jumps over the lazy dog. This sentence contains every letter in the English alphabet. "
-    "The quick brown fox jumps over the lazy dog. This sentence contains every letter in the English alphabet. "
-    "The quick brown fox jumps over the lazy dog. This sentence contains every letter in the English alphabet. "
-    "The quick brown fox jumps over the lazy dog. This sentence contains every letter in the English alphabet. "
-    "The quick brown fox jumps over the lazy dog. This sentence contains every letter in the English alphabet. "
-    "The quick brown fox jumps over the lazy dog. This sentence contains every letter in the English alphabet. "
-    "The quick brown fox jumps over the lazy dog. This sentence contains every letter in the English alphabet. "
-    "The quick brown fox jumps over the lazy dog. This sentence contains every letter in the English alphabet. "
-    "Huffman coding will compress this text more efficiently if some letters appear more frequently. Let's see!";
-
-
-
-    huffman.Test(text);
-
+    std::string text = "Hello, World!";
+    std::cout << text << std::endl;
     return 0;
 }
