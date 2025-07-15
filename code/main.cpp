4a300a4055fe935357ee45d4390e713117390b22
Dan Adrian Mirea <damirea84@gmail.com>
2025-07-15 23:44:10 +0300

    cleanup main

--- code/main.cpp
+++ code/main.cpp
@@ -1,9 +1,5 @@
 #include <iostream>
-#include "strongEnums.hpp"
 
 int main() {
-    // Demonstrate strongly typed enums
-    strong_enum_examples::demonstrate_strong_enums();
-    
     return 0;
 }
