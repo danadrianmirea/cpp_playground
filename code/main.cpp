642af87ce98042a9843296f32baf45537d0c1202
Dan Adrian Mirea <damirea84@gmail.com>
2025-07-15 21:05:18 +0300

    cleanup main

--- code/main.cpp
+++ code/main.cpp
@@ -1,9 +1,5 @@
 #include <iostream>
-#include "auto_keyword.hpp"
 
 int main() {
-    // Demonstrate the auto keyword examples
-    auto_examples::demonstrate_auto_keyword();
-    
     return 0;
 }
