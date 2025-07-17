94429c14fc784ac808bbf925fba7f45a4303fd2a
Dan Adrian Mirea <damirea84@gmail.com>
2025-07-17 21:09:20 +0300

    add char lvalue/rvalue example

--- code/main.cpp
+++ code/main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
 
+void print(char* &&) { std::cout << "1" << std::endl; }
+void print(char* &) { std::cout << "2" << std::endl; }
+
+void testRValueLValue()
+{
+    char a = 'a';
+    char* b = &a;
+    print(&a);
+    print(b);
+}
+
 int main() {
+    testRValueLValue();
     return 0;
 }
