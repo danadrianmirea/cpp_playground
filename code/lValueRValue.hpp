8f881f064a0512fcf3cd084905e593f1c81e2abe
Dan Adrian Mirea <damirea84@gmail.com>
2025-07-17 21:10:06 +0300

    move lvalue interview question code to its own file

---
+++ code/lValueRValue.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+void print(char* &&) { std::cout << "1" << std::endl; }
+void print(char* &) { std::cout << "2" << std::endl; }
+
+void testRValueLValue()
+{
+    char a = 'a';
+    char* b = &a;
+    print(&a);
+    print(b);
+}
--- code/main.cpp
+++ code/main.cpp
@@ -1,15 +1,5 @@
 #include <iostream>
-
-void print(char* &&) { std::cout << "1" << std::endl; }
-void print(char* &) { std::cout << "2" << std::endl; }
-
-void testRValueLValue()
-{
-    char a = 'a';
-    char* b = &a;
-    print(&a);
-    print(b);
-}
+#include "lValueRValue.hpp"
 
 int main() {
     testRValueLValue();
