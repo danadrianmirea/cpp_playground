ad7f4b29e8074c3ff7ce49e1ba3df52b493f90a7
Dan Adrian Mirea <damirea84@gmail.com>
2026-03-21 22:44:31 +0200

    add proper code to generate valid and invalid 16 digit card numbers

--- code/verifyCreditCardNumber.hpp
+++ code/verifyCreditCardNumber.hpp
@@ -1,42 +1,63 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <cctype>
+#include <cstdlib>
+#include <ctime>
 
+// Luhn check
 bool isValidCreditCard(const std::string& cardNumber) {
     if (cardNumber.length() != 16) return false;
-
     int sum = 0;
     for (char c : cardNumber) {
-        if (!std::isdigit(c)) return false;
+        if (!isdigit(c)) return false;
     }
-
     for (int i = 0; i < 16; ++i) {
-        int digit = cardNumber[15 - i] - '0';  // Start from rightmost digit
+        int digit = cardNumber[15 - i] - '0';
         if (i % 2 == 1) {
             digit *= 2;
             if (digit > 9) digit -= 9;
         }
         sum += digit;
     }
-
     return (sum % 10 == 0);
 }
 
+// Generate a valid 16-digit number
+std::string generateValidCard() {
+    std::string num = "";
+    for (int i = 0; i < 15; ++i) {
+        num += '0' + rand() % 10;
+    }
+
+    // Compute last digit (check digit) using Luhn
+    int sum = 0;
+    for (int i = 0; i < 15; ++i) {
+        int digit = num[14 - i] - '0';
+        if (i % 2 == 0) digit *= 2;
+        if (digit > 9) digit -= 9;
+        sum += digit;
+    }
+    int checkDigit = (10 - (sum % 10)) % 10;
+    num += '0' + checkDigit;
+    return num;
+}
+
+// Generate an invalid 16-digit number (just change last digit of a valid one)
+std::string generateInvalidCard() {
+    std::string valid = generateValidCard();
+    valid[15] = (valid[15] - '0' + 1) % 10 + '0'; // change last digit
+    return valid;
+}
+
 int verifyCreditCardNumberDemo() {
-    // Test data: 5 valid, 5 invalid (all 16-digit)
-    std::vector<std::string> testCards = {
-        "4532015112830366", // valid
-        "6011111111111117", // valid
-        "4916733930467000", // valid (16-digit replacement for AMEX)
-        "5555555555554444", // valid
-        "4111111111111111", // valid
-        "1234567812345678", // invalid
-        "0000000000000000", // invalid
-        "4111111111111121", // invalid
-        "5500000000000004", // invalid
-        "6011000000000001"  // invalid
-    };
+    srand((unsigned)time(0));
+
+    std::vector<std::string> testCards;
+
+    // Generate 5 valid cards
+    for (int i = 0; i < 5; ++i) testCards.push_back(generateValidCard());
+    // Generate 5 invalid cards
+    for (int i = 0; i < 5; ++i) testCards.push_back(generateInvalidCard());
 
     for (const auto& card : testCards) {
         std::cout << card << " : "
